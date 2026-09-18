828b13706787030f9ad182d99ee1e7563f497e9b
Kaique Alves Fernandes <kaique.fernandes@estudante.ifms.edu.br>
2026-09-17 21:14:37 -0400

    Integrate Swagger UI for API documentation
    Added Swagger UI for API documentation.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,6 @@
 import express, { type Application } from "express";
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger';
 
 export function createApp(): Application {
   const app = express();
@@ -7,9 +9,19 @@ export function createApp(): Application {
   app.disable("x-powered-by");
   app.use(express.json());
 
+  app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+ );
+ 
   // Routes
   app.get("/", (_, res) => {
     res.json({ message: "Hello, World" });
+  });
+
+  app.get('/docs.json', (_req, res) => {
+    res.json(swaggerSpec);
   });
 
   return app;

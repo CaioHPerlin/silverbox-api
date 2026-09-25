c6f26374f4c128c9eb4ec121a8d7ce09b5f9dea4
caiohperlin <caiohperlin@gmail.com>
2026-09-25 19:35:13 -0400

    chore: update dependency tree and allow CI to pass

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Application } from "express";
 import { loggerMiddleware } from "./middleware/logger.js";
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger.js';
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
 
 export function createApp(): Application {
   const app = express();
@@ -12,18 +12,15 @@ export function createApp(): Application {
   app.disable("x-powered-by");
   app.use(express.json());
 
-  app.use(
-  '/docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec)
- );
- 
   // Routes
+
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
   app.get("/", (_, res) => {
     res.json({ message: "Hello, World" });
   });
 
-  app.get('/docs.json', (_req, res) => {
+  app.get("/docs.json", (_req, res) => {
     res.json(swaggerSpec);
   });
 
